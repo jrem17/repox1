93de3df887e016e4432ba65adf8e9b820a7a8034
Jozef Remenec <jre@whitestein.com>
2015-03-29 16:39:20 +0200

    Adding Notes1

--- src/com/cerberus/HelloWorld.java
+++ src/com/cerberus/HelloWorld.java
@@ -6,13 +6,14 @@ import javafx.scene.control.Label;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-// TT
+// TT --
 public class HelloWorld extends Application {
 	/*
 	 * (non-Javadoc)
 	 * @see javafx.application.Application#start(javafx.stage.Stage)
 	 */
 	public void start(Stage stage) {
+		
 		Label message = new Label("Hello, JavaFX!");
 		message.setFont(new Font(100));
 		stage.setScene(new Scene(message));
---
+++ src/com/cerberus/Notes1.java
@@ -0,0 +1,9 @@
+package com.cerberus;
+
+public class Notes1 {
+	public static void main(String[] args) {
+		Object o = "stringerex";
+
+		System.out.println(o);
+	}
+}
